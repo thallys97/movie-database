60b89513e577d472f2c3a2a44a5bd656b7d08d83
thallys97 <thallysbandeira97@gmail.com>
2024-03-12 15:13:46 -0500

    corrigi a criação do nome do controller para incluir o namespace completo e verificando se a classe do controller existe

--- public/index.php
+++ public/index.php
@@ -3,18 +3,22 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $url = $_GET['url'] ?? 'movies/index';
 $url = explode('/', $url);
-$controllerName = ucfirst($url[0]).'Controller';
-$method = isset($url[1]) ? $url[1] : 'index';
 
-if(file_exists("../app/controllers/{$controllerName}.php")) {
-    require_once "../app/controllers/{$controllerName}.php";
-    $controller = new $controllerName;
+// Corrige a criação do nome do controller para incluir o namespace completo
+$controllerName = 'App\\Controllers\\' . ucfirst($url[0]) . 'Controller';
+$method = $url[1] ?? 'index';
+
+// Verifica se a classe do controller existe (não é mais necessário verificar se o arquivo existe)
+if(class_exists($controllerName)) {
+    $controller = new $controllerName();
 
     if(method_exists($controller, $method)) {
         call_user_func_array([$controller, $method], []);
     } else {
         // Método não encontrado
+        echo "Método {$method} não encontrado no controller {$controllerName}.";
     }
 } else {
     // Controller não encontrado
+    echo "Controller {$controllerName} não encontrado.";
 }
