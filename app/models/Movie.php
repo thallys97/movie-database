32d146d667dcc456d6ed296eee4408f172403b2f
thallys97 <thallysbandeira97@gmail.com>
2024-03-12 15:11:38 -0500

    alterei a model para usar a API do TMDB para buscar os filmes

--- app/models/Movie.php
+++ app/models/Movie.php
@@ -2,17 +2,43 @@
 
 namespace App\Models;
 
-use App\Database\Database;
+//use App\Database\Database;
+
+use Exception;
 
 class Movie {
-    private $db;
+    //private $db;
+    private $apiKey = '25bbb10392bc8295016e07f3c4804105'; // Substitua por sua chave API da TMDB
 
-    public function __construct() {
-        $this->db = new Database();
-    }
+    // public function __construct() {
+    //     $this->db = new Database();
+    // }
 
-    public function getLatestMovies() {
-        $this->db->query("SELECT * FROM movies ORDER BY release_date DESC LIMIT 20");
-        return $this->db->resultSet();
+    // public function getLatestMovies() {
+    //     $this->db->query("SELECT * FROM movies ORDER BY release_date DESC LIMIT 20");
+    //     return $this->db->resultSet();
+    // }
+
+    public function fetchLatestMoviesFromTMDB() {
+        $url = "https://api.themoviedb.org/3/movie/now_playing?api_key={$this->apiKey}&language=pt-BR";
+
+        try {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $result = curl_exec($ch);
+            curl_close($ch);
+
+            $data = json_decode($result, true);
+
+            // Garantir que a resposta contém a chave 'results' que é esperada ser um array de filmes
+            if (isset($data['results'])) {
+                return $data['results'];
+            } else {
+                return []; // Retorna um array vazio caso não existam resultados
+            }
+        } catch (Exception $e) {
+            return ['error' => $e->getMessage()];
+        }
     }
 }
