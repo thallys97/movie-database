f8d329efb3f48b74c0acb73c826d2df8879dbe60
thallys97 <thallysbandeira97@gmail.com>
2024-03-12 15:12:49 -0500

    alterei a view para garantir que a chave 'title' existe para o nome do filme poder ser exibido

--- app/views/latestMovies.php
+++ app/views/latestMovies.php
@@ -1,6 +1,12 @@
-<?php foreach($data['movies'] as $movie): ?>
-    <div class="movie">
-        <h3><?= $movie->title; ?></h3>
-        <!-- Outras informações do filme -->
-    </div>
-<?php endforeach; ?>
+<?php if (isset($data['tmdbMovies']) && is_array($data['tmdbMovies'])): ?>
+    <?php foreach ($data['tmdbMovies'] as $movie): ?>
+        <?php if (isset($movie['title'])): // Certifique-se de que a chave 'title' existe ?>
+            <div class="movie">
+                <h3><?= htmlspecialchars($movie['title']); ?></h3>
+                <!-- Outras informações do filme -->
+            </div>
+        <?php endif; ?>
+    <?php endforeach; ?>
+<?php else: ?>
+    <p>Nenhum filme encontrado.</p>
+<?php endif; ?>
