e61e639e2d8199f099555eadf32d923be6386b90
thallys97 <thallysbandeira97@gmail.com>
2024-03-13 18:51:50 -0500

    adicionei o método bind no arquivo de conexão com a database

--- app/database/Database.php
+++ app/database/Database.php
@@ -32,6 +32,25 @@ class Database {
         $this->stmt = $this->connect()->prepare($sql);
     }
 
+    public function bind($param, $value, $type = null) {
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+        $this->stmt->bindValue($param, $value, $type);
+    }
+
     public function execute() {
         return $this->stmt->execute();
     }
