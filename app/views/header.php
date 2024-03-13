14ec096a4ccf768fa09fec30e9eb2bdc5a9e33fa
thallys97 <thallysbandeira97@gmail.com>
2024-03-13 18:52:23 -0500

    criei a view que contém o cabeçalho

---
+++ app/views/header.php
@@ -0,0 +1,11 @@
+<nav class="bg-gray-800 p-4 text-white flex justify-between">
+        <div class="font-bold"><a href="/">Movie App</a></div>
+        <div>
+            <?php if (isset($_SESSION['user_id'])): ?>
+                <a href="/logout" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Logout</a>
+            <?php else: ?>
+                <a href="/login" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login</a>
+                <a href="/register" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Register</a>
+            <?php endif; ?>
+        </div>
+    </nav>
