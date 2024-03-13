e62d2e44440bd15e60f258a0d54565539a912074
thallys97 <thallysbandeira97@gmail.com>
2024-03-13 10:53:29 -0500

    acrescentei um método na model de filme para pegar detalhes de um determinado filme

--- app/models/Movie.php
+++ app/models/Movie.php
@@ -67,4 +67,23 @@ class Movie {
         }
     }
 
+    public function fetchMovieDetailsFromTMDB($id) {
+        $url = "https://api.themoviedb.org/3/movie/{$id}?api_key={$this->apiKey}&language=pt-BR&append_to_response=credits,release_dates";
+    
+        try {
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                $result = curl_exec($ch);
+                curl_close($ch);
+    
+                $data = json_decode($result, true);
+
+            return $data;
+
+        } catch (Exception $e) {
+            return ['error' => $e->getMessage()];
+        }
+    }
+
 }
