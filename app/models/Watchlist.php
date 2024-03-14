eaef6cbdcb2887ceffa110eea8e9235c4dd14718
thallys97 <thallysbandeira97@gmail.com>
2024-03-14 18:09:36 -0500

    atualizei o método addMovieToWatchlist

--- app/models/Watchlist.php
+++ app/models/Watchlist.php
@@ -12,11 +12,26 @@ class Watchlist {
     }
 
     public function addMovieToWatchlist($userId, $tmdbMovieId) {
-        $this->db->query("INSERT INTO Watchlist (user_id, tmdb_movie_id) VALUES (:user_id, :tmdb_movie_id)");
+        // Primeiro, verifique se o filme já está na watchlist
+        $this->db->query("SELECT 1 FROM Watchlist WHERE user_id = :user_id AND tmdb_movie_id = :tmdb_movie_id");
         $this->db->bind(':user_id', $userId);
         $this->db->bind(':tmdb_movie_id', $tmdbMovieId);
-        return $this->db->execute();
+    
+        if ($this->db->single()) {
+            // O filme já está na watchlist, não faça nada
+            return false;
+        } else {
+            // Insira o novo filme na watchlist
+            $this->db->query("INSERT INTO Watchlist (user_id, tmdb_movie_id) VALUES (:user_id, :tmdb_movie_id)");
+            // Rebind os parâmetros para a nova consulta
+            $this->db->bind(':user_id', $userId);
+            $this->db->bind(':tmdb_movie_id', $tmdbMovieId);
+    
+            // Execute a inserção
+            return $this->db->execute();
+        }
     }
+    
 
     public function getUserWatchlist($userId) {
         $this->db->query("SELECT tmdb_movie_id FROM Watchlist WHERE user_id = :user_id");
