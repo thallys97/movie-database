44731b70a9bfe670712046d890000ba0d395cc47
thallys97 <thallysbandeira97@gmail.com>
2024-03-14 12:09:31 -0500

    ajeitei o cabeçalho para incluir item da watchlist

--- app/views/header.php
+++ app/views/header.php
@@ -2,10 +2,11 @@
         <div class="font-bold"><a href="/">Movie App</a></div>
         <div>
             <?php if (isset($_SESSION['user_id'])): ?>
+                <a href="/watchlist" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">Minha Watchlist</a>
                 <a href="/logout" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Logout</a>
             <?php else: ?>
                 <a href="/login" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login</a>
-                <a href="/register" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Register</a>
+                <a href="/register" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Registro</a>
             <?php endif; ?>
         </div>
     </nav>
