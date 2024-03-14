eb3a05dd489793d6acd8c59e5b6c4fc6c098cf18
thallys97 <thallysbandeira97@gmail.com>
2024-03-14 16:05:30 -0500

    alterei a view de watchlist para mostrar os filmes da watchlist

--- app/views/watchlist.php
+++ app/views/watchlist.php
@@ -13,16 +13,21 @@
 
 <div class="container mx-auto px-4 py-8">
     <h1 class="text-4xl font-bold mt-2 md:mt-0">Minha Watchlist</h1>
-    <?php if (empty($data['watchlist'])): ?>
-        <div class="mt-4">
-            <p class="text-xl">Nenhum filme foi adicionado na watchlist.</p>
-        </div>
+    <?php if (empty($data['watchlistMovies'])): ?>
+    <div class="mt-4">
+        <p class="text-xl">Nenhum filme foi adicionado na watchlist.</p>
+    </div>
     <?php else: ?>
-        <div>
-            <!-- Aqui você irá listar os filmes da watchlist -->
-            <?php foreach ($data['watchlist'] as $movie): ?>
-                <!-- Exemplo simples apenas mostrando o ID do filme -->
-                <p>Filme TMDB ID: <?= htmlspecialchars($movie->tmdb_movie_id) ?></p>
+        <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            <?php foreach ($data['watchlistMovies'] as $movie): ?>
+                <div class="card">
+                    <img src="https://image.tmdb.org/t/p/w500<?= $movie['poster_path'] ?>" alt="<?= $movie['title'] ?>" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= htmlspecialchars($movie['title']) ?></h5>
+                        <p class="card-text"><?= implode(', ', array_column($movie['genres'], 'name')) ?></p>
+                        <p class="card-text">Avaliação: <?= htmlspecialchars($movie['vote_average']) ?>/10</p>
+                    </div>
+                </div>
             <?php endforeach; ?>
         </div>
     <?php endif; ?>
