525b74fef26c514564552b690d410ad49538baa7
thallys97 <thallysbandeira97@gmail.com>
2024-03-15 10:56:39 -0500

    adicionei na model o método de deletar da watchlist

--- app/models/Watchlist.php
+++ app/models/Watchlist.php
@@ -31,7 +31,13 @@ class Watchlist {
             return $this->db->execute();
         }
     }
-    
+
+    public function removeMovieFromWatchlist($userId, $tmdbMovieId) {
+        $this->db->query("DELETE FROM Watchlist WHERE user_id = :user_id AND tmdb_movie_id = :tmdb_movie_id");
+        $this->db->bind(':user_id', $userId);
+        $this->db->bind(':tmdb_movie_id', $tmdbMovieId);
+        return $this->db->execute();
+    }
 
     public function getUserWatchlist($userId) {
         $this->db->query("SELECT tmdb_movie_id FROM Watchlist WHERE user_id = :user_id");
