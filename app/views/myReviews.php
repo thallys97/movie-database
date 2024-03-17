90cc80e8529b1dc865e451bebf0bb0c32aa21243
thallys97 <thallysbandeira97@gmail.com>
2024-03-17 11:34:14 -0500

    coloquei cada card de review em uma âncora

--- app/views/myReviews.php
+++ app/views/myReviews.php
@@ -14,23 +14,25 @@
     <h1 class="text-4xl font-bold">Minhas Reviews</h1>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <?php foreach ($data['myReviews'] as $review): ?>
-            <div class="review-card bg-white shadow-lg rounded-lg p-4">
-                <?php foreach ($data['reviewMoviesDetails'] as $movieDetail): ?>
-                    <?php if ($movieDetail['id'] == $review->tmdb_movie_id): ?>
-                        <!-- <img src="https://image.tmdb.org/t/p/w500<?= $movieDetail['poster_path'] ?>" alt="Poster" class="rounded-lg"> -->
-                        
-                        <h2 class="text-xl font-bold">
-                            <?= $movieDetail['title'] . ' (' . date('Y', strtotime($movieDetail['release_date'])) . ')' ?>
-                        </h2>
-                    <?php endif; ?> 
-                <?php endforeach; ?>        
-                <div class="review-content p-4">
-                    <?php if (!is_null($review->rating)): ?>
-                        <p>Minha Avaliação: <?= htmlspecialchars($review->rating) ?>/10</p>
-                    <?php endif; ?>
-                    <p><?= htmlspecialchars($review->comment) ?></p>
+            <a href="/review/<?= $review->tmdb_movie_id ?>">
+                <div class="review-card bg-white shadow-lg rounded-lg p-4">
+                    <?php foreach ($data['reviewMoviesDetails'] as $movieDetail): ?>
+                        <?php if ($movieDetail['id'] == $review->tmdb_movie_id): ?>
+                            <!-- <img src="https://image.tmdb.org/t/p/w500<?= $movieDetail['poster_path'] ?>" alt="Poster" class="rounded-lg"> -->
+                            
+                            <h2 class="text-xl font-bold">
+                                <?= $movieDetail['title'] . ' (' . date('Y', strtotime($movieDetail['release_date'])) . ')' ?>
+                            </h2>
+                        <?php endif; ?> 
+                    <?php endforeach; ?>        
+                    <div class="review-content p-4">
+                        <?php if (!is_null($review->rating)): ?>
+                            <p>Minha Avaliação: <?= htmlspecialchars($review->rating) ?>/10</p>
+                        <?php endif; ?>
+                        <p><?= htmlspecialchars($review->comment) ?></p>
+                    </div>
                 </div>
-            </div>
+            </a>
         <?php endforeach; ?>
     </div>
 </div>
