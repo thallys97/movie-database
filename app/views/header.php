dfdcda3738fec17c15edb1fe9ad9515a02b73407
thallys97 <thallysbandeira97@gmail.com>
2024-03-19 18:41:04 -0500

    apaguei o código antigo do header

--- app/views/header.php
+++ app/views/header.php
@@ -1,16 +1,4 @@
-<!-- <nav class="bg-gray-800 p-4 text-white flex justify-between">
-        <div class="font-bold"><a href="/">Movie App</a></div>
-        <div>
-            <?php if (isset($_SESSION['user_id'])): ?>
-                <a href="/my-reviews" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">Reviews</a>
-                <a href="/watchlist" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">Watchlist</a>
-                <a href="/logout" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Logout</a>
-            <?php else: ?>
-                <a href="/login" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login</a>
-                <a href="/register" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Registro</a>
-            <?php endif; ?>
-        </div>
-    </nav> -->
+
 
     <nav class="bg-gray-800 p-4 text-white">
     <div class="flex items-center justify-between flex-wrap">
